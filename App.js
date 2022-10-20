7a3acb52940092eba62acae06233035df77db39c
Chinmay Nilkanth <88083543+chinmay770@users.noreply.github.com>
2022-10-21 00:40:47 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,87 +1,193 @@
-import { Routes, Route } from "react-router-dom";
+import "antd/dist/antd.css";
 import "./App.css";
-import allRoutes from "./data/routes";
+import { Button, Table, Modal, Input } from "antd";
+import { useState } from "react";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Anchor } from 'antd';
+import React from 'react';
+import allRoute from './route';
 
-//Main Components Imports
-import Main from "./Components/Home/Main";
-
-//Login Components Import
-
-//class-cc module imports
-import ClassCCLogin from "./Components/ClassCC/Login";
-import ClassCCRegister from "./Components/ClassCC/Signup";
-import ClassCCDashboard from "./Components/ClassCC/Dashboard";
-
-//mentor module imports
-import MentorLogin from "./Components/Mentor/Login";
-import MentorRegister from "./Components/Mentor/Signup";
-import MentorDashboard from "./Components/Mentor/Dashboard";
-import MentorCounseling from "./Components/Mentor/Layout/Counseling";
-import MentorDashboard2 from "./Components/Mentor/Layout/Dashboard";
-import MentorMessage from "./Components/Mentor/Layout/Message";
-import MentorProfile from "./Components/Mentor/Layout/Profile";
-import MentorStudent from "./Components/Mentor/Layout/Student";
-
-//Student module imports
-import StudentLogin from "./Components/Students/Login";
-import StudentRegister from "./Components/Students/Signup";
-import StudentDashboard from "./Components/Students/Layout/DashBoard";
-import StudentAcademic from "./Components/Students/Layout/Academic";
-import StudentAchievement from "./Components/Students/Layout/Achievement";
-import StudentProfile from "./Components/Students/Layout/Profile";
-import StudentSemester from "./Components/Students/Layout/Semester";
-import StudentMessage from "./Components/Students/Layout/Message";
-import StudentForgotPassword from "./Components/Students/ForgotPassword";
-import StudentResetPassword from "./Components/Students/ResetPassword";
+const { Link } = Anchor;
 
 function App() {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editingStudent, setEditingStudent] = useState(null);
+  const [dataSource, setDataSource] = useState([
+    {
+      id: 1,
+      name: "Chinmay",
+      email: "Chinmay@gmail.com",
+      Branch: "IT",
+    },
+    {
+      id: 2,
+      name: "Dipesh",
+      email: "Dipesh@gmail.com",
+      Branch: "IT",
+    },
+    {
+      id: 3,
+      name: "Arjun",
+      email: "Arjun@gmail.com",
+      Branch: "IT",
+    },
+    {
+      id: 4,
+      name: "Akhilesh",
+      email: "Akhilesh@gmail.com",
+      Branch: "IT",
+    },
+  ]);
+  const columns = [
+    {
+      key: "1",
+      title: "ID",
+      dataIndex: "id",
+    },
+    {
+      key: "2",
+      title: "Name",
+      dataIndex: "name",
+    },
+    {
+      key: "3",
+      title: "Email",
+      dataIndex: "email",
+    },
+    {
+      key: "4",
+      title: "Branch",
+      dataIndex: "Branch",
+    },
+    {
+      key: "5",
+      title: "Actions",
+      render: (record) => {
+        return (
+          <>
+            <EditOutlined
+              onClick={() => {
+                onEditStudent(record);
+              }}
+            />
+            <DeleteOutlined
+              onClick={() => {
+                onDeleteStudent(record);
+              }}
+              style={{ color: "red", marginLeft: 12 }}
+            />
+           
+
+          </>
+        );
+      },
+    },
+
+    {
+      key: "6",
+      title: "View",
+      render: (record) =>{
+        return(
+          <Anchor>
+            <Link href={allRoute.view} title="View" /> 
+          </Anchor>
+        )
+      }
+
+    }
+  ];
+
+  const onAddStudent = () => {
+    const randomNumber = parseInt(Math.random() * 1000);
+    const newStudent = {
+      id: randomNumber,
+      name: "Name ",
+      email: randomNumber + "@gmail.com",
+      Branch: "Branch",
+    };
+    setDataSource((pre) => {
+      return [...pre, newStudent];
+    });
+  };
+  const onDeleteStudent = (record) => {
+    Modal.confirm({
+      title: "Are you sure, you want to delete this student record?",
+      okText: "Yes",
+      okType: "danger",
+      onOk: () => {
+        setDataSource((pre) => {
+          return pre.filter((student) => student.id !== record.id);
+        });
+      },
+    });
+  };
+  const onEditStudent = (record) => {
+    setIsEditing(true);
+    setEditingStudent({ ...record });
+  };
+  const resetEditing = () => {
+    setIsEditing(false);
+    setEditingStudent(null);
+  };
   return (
     <div className="App">
-      <Routes>
-        {/* Landing Main Pages */}
-        <Route path="/" element={<Main />} />
-
-        {/* Class-CC Pages */}
-        <Route path={allRoutes.ccLogin} element={<ClassCCLogin />} />
-        <Route path={allRoutes.ccRegister} element={<ClassCCRegister />} />
-        <Route path={allRoutes.ccRegister} element={<ClassCCDashboard />} />
-
-        {/* Mentor Pages */}
-        <Route path={allRoutes.mentorLogin} element={<MentorLogin />} />
-        <Route path={allRoutes.mentorRegister} element={<MentorRegister />} />
-        <Route path={allRoutes.mentorDashboard} element={<MentorDashboard />} />
-
-        {/* Student Pages */}
-        <Route path={allRoutes.studentLogin} element={<StudentLogin />} />
-        <Route path={allRoutes.studentRegister} element={<StudentRegister />} />
-        <Route
-          path={allRoutes.studentDashboard}
-          element={<StudentDashboard />}
-        />
-        <Route
-          path={allRoutes.studentDashboard}
-          element={<StudentDashboard />}
-        />
-        <Route path={allRoutes.studentAcademic} element={<StudentAcademic />} />
-        <Route
-          path={allRoutes.studentAllSemesterDetails}
-          element={<StudentSemester />}
-        />
-        <Route
-          path={allRoutes.studentAchievements}
-          element={<StudentAchievement />}
-        />
-        <Route path={allRoutes.studentProfile} element={<StudentProfile />} />
-        <Route path={allRoutes.studentMessage} element={<StudentMessage />} />
-        <Route
-          path={allRoutes.studentForgotPassword}
-          element={<StudentForgotPassword />}
-        />
-        <Route
-          path={allRoutes.studentResetPassword}
-          element={<StudentResetPassword />}
-        />
-      </Routes>
+      <header className="App-header">
+        <Button onClick={onAddStudent}>Add a new Student</Button>
+        <Table columns={columns} dataSource={dataSource}></Table>
+        <Modal
+          title="Edit Student"
+          visible={isEditing}
+          okText="Save"
+          onCancel={() => {
+            resetEditing();
+          }}
+          onOk={() => {
+            setDataSource((pre) => {
+              return pre.map((student) => {
+                if (student.id === editingStudent.id) {
+                  return editingStudent;
+                } else {
+                  return student;
+                }
+              });
+            });
+            resetEditing();
+          }}
+        >
+          <Input
+            value={editingStudent?.id}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, id: e.target.value };
+              });
+            }}
+          />
+          <Input
+            value={editingStudent?.name}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, name: e.target.value };
+              });
+            }}
+          />
+          <Input
+            value={editingStudent?.email}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, email: e.target.value };
+              });
+            }}
+          />
+          <Input
+            value={editingStudent?.Branch}
+            onChange={(e) => {
+              setEditingStudent((pre) => {
+                return { ...pre, Branch: e.target.value };
+              });
+            }}
+          />
+        </Modal>
+      </header>
     </div>
   );
 }
---
+++ route.js
@@ -0,0 +1,3 @@
+const allRoute = {
+view: "/view",
+}
